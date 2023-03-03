67c76d70658494702a98b7c555c9fb690912eb4e
FAForhad <faforhad450@gmail.com>
2023-03-03 20:10:06 +0600

    dropdown

--- src/Pages/Shared/Navber/Navber.js
+++ src/Pages/Shared/Navber/Navber.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
 
 const Navber = () => {
   const menu = (
@@ -29,15 +30,6 @@ const Navber = () => {
           className="lg:px-20 px-12 py-4 hover:bg-gray-900  hover:text-lime-400 lg:border-r border-solid border-slate-900 text-md transition duration-300 "
         >
           Man
-        </Link>
-      </li>
-      <li className="rounded-none">
-        <Link
-          to="/"
-          title="New Products"
-          className="lg:px-20 px-12  uppercase lg:border-r border-solid border-slate-900 text-md"
-        >
-          New
         </Link>
       </li>
     </>
@@ -78,7 +70,35 @@ const Navber = () => {
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className=" menu-horizontal px-1">{menu}</ul>
+          <ul className=" menu-horizontal px-1">
+            {menu}
+            <li className="rounded-none lg:px-20 flex items-center">
+              <Link to="/" title="New Products" className="text-md">
+                <div className="dropdown dropdown-hover">
+                  <label tabIndex={0} className="rounded-full cursor-pointer">
+                    <CgProfile />
+                  </label>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+                  >
+                    <li>
+                      <a>My Orders</a>
+                    </li>
+                    <li>
+                      <a>My Addresses</a>
+                    </li>
+                  </ul>
+                </div>
+              </Link>
+              <Link to="/" title="New Products" className="px-2 text-md">
+                Log In
+              </Link>
+              <Link to="/" title="New Products" className="text-md">
+                New
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
