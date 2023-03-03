43c05c1c93e5e48a486e9070d57b0ca324f0c723
FAForhad <faforhad450@gmail.com>
2023-03-03 18:05:56 +0600

    navber

--- src/Pages/Shared/Navber/Navber.js
+++ src/Pages/Shared/Navber/Navber.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navber = () => {
+  const menu = (
+    <>
+      <li className="rounded-none">
+        <Link
+          to="/"
+          title="New Products"
+          className="lg:px-20 px-12 py-4 uppercase lg:border-r border-solid border-slate-900 text-md"
+        >
+          New
+        </Link>
+      </li>
+      <li className="rounded-none">
+        <Link
+          to="/"
+          title="New Products"
+          className="lg:px-20 px-12 py-4 uppercase lg:border-r border-solid border-slate-900 text-md"
+        >
+          WoMan
+        </Link>
+      </li>
+      <li className="rounded-none">
+        <Link
+          to="/"
+          title="New Products"
+          className="lg:px-20 px-12 py-4 hover:bg-gray-900  hover:text-lime-400 lg:border-r border-solid border-slate-900 text-md"
+        >
+          Man
+        </Link>
+      </li>
+      <li className="rounded-none">
+        <Link
+          to="/"
+          title="New Products"
+          className="lg:px-20 px-12  uppercase lg:border-r border-solid border-slate-900 text-md"
+        >
+          New
+        </Link>
+      </li>
+    </>
+  );
   return (
     <div>
-      <div className="navbar py-3 px-0 border-b border-slate-900 border-solid bg-base-100 ">
+      <div className="navbar justify-between py-3 px-0 border-b border-slate-900 border-solid bg-base-100 ">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -26,71 +67,18 @@ const Navber = () => {
               tabIndex={0}
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menu}
             </ul>
           </div>
-          <button className="px-24 h-full py-0 uppercase border-r border-solid border-x-slate-900 font-bold text-2xl">
+          <Link
+            to="/"
+            className="lg:px-24 px-12 py-3 uppercase lg:border-r border-solid border-x-slate-900 font-bold text-2xl"
+          >
             Bliss
-          </button>
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className=" menu-horizontal px-1">{menu}</ul>
         </div>
       </div>
     </div>
