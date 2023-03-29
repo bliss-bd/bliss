751e83a2ad904020739c1555db02adc5874528b5
FAForhad <faforhad450@gmail.com>
2023-03-29 15:01:29 +0600

    discount_section_complete

--- src/Pages/Home/Discount/Discount.js
+++ src/Pages/Home/Discount/Discount.js
@@ -1,20 +1,33 @@
 import React from "react";
 import RAF from "../../../assets/RAF.jpg";
 import { BsLightningChargeFill } from "react-icons/bs";
-
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 const Discount = () => {
   return (
-    <div className="my-12 grid grid-cols-3 ">
-      <div className="col-span-2 text-center sm:text-left pt-64 from-lime-300 h-full  bg-gradient-to-t bg-opacity-75 ">
-        <BsLightningChargeFill className="text-center mx-auto w-20 h-16 text-black"></BsLightningChargeFill>
-        <p className="hidden text-gray-700 text-center text-2xl md:mt-4 md:block">SALE IS ON!</p>
-        <h2 className="text-2xl font-bold text-black text-center md:text-9xl">25% OFF</h2>
-        <p className="hidden text-gray-700 text-center text-xl md:mt-4 md:block">
-          25% off sitewide using TEES25 at checkout
-        </p>
+    <div>
+      <div className="mt-12 grid lg:grid-cols-3 md:grid-cols-2">
+        <div className="lg:col-span-2 text-center sm:text-left py-14 lg:pt-64 from-lime-300 h-full  bg-gradient-to-t bg-opacity-75 ">
+          <BsLightningChargeFill className="text-center mx-auto w-20 h-16 text-black"></BsLightningChargeFill>
+          <p className="hidden text-gray-700 text-center text-2xl md:mt-4 md:block">SALE IS ON!</p>
+          <h2 className="text-2xl font-bold text-black text-center md:text-9xl">25% OFF</h2>
+          <p className="hidden text-gray-700 text-center text-xl md:mt-4 md:block">
+            25% off sitewide using TEES25 at checkout
+          </p>
+        </div>
+        <div>
+          <img alt="Violin" src={RAF} className="md:rounded-tl-[60px]" />
+        </div>
       </div>
-      <div>
-        <img alt="Violin" src={RAF} className=" md:rounded-tl-[60px]" />
+      <div className="my-12">
+        <h1 className="flex lg:gap-8 mx-1 items-center justify-center text-4xl font-bold text-black underline">
+          <span>
+            <FaLongArrowAltRight className="w-16 h-16"></FaLongArrowAltRight>
+          </span>
+          GET 10% OFF YOUR FIRST ORDER!
+          <span>
+            <FaLongArrowAltLeft className="w-16 h-16"></FaLongArrowAltLeft>
+          </span>
+        </h1>
       </div>
     </div>
   );
