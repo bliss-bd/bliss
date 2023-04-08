77a797022d32430493a10db97ae9f41cd5556b02
FAForhad <faforhad450@gmail.com>
2023-04-08 16:51:27 +0600

    shop_new_data_load

--- src/Pages/Shop/ShopNew/ShopNew.js
+++ src/Pages/Shop/ShopNew/ShopNew.js
@@ -1,7 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import men from "../../../assets/men.jpg";
+import LoaderCard from "../../../Components/LoaderCard/LoaderCard";
+import NewDrop from "../../Home/NewDrop/NewDrop";
 
 const ShopNew = () => {
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/products");
+      const data = await res.json();
+      return data;
+    },
+  });
+  if (isLoading) {
+    return <LoaderCard></LoaderCard>;
+  }
   return (
     <div className="mb-8">
       <header className="text-center">
@@ -11,174 +24,9 @@ const ShopNew = () => {
       </header>
 
       <div className="grid gap-14 sm:grid-cols-2 lg:grid-cols-4 lg:mx-14 mx-6 py-6 sm:px-6 sm:py-8 lg:px-8">
-        <div>
-          <a href="#" className="block overflow-hidden group">
-            <img
-              src={men}
-              alt=""
-              className="h-[350px] w-full object-cover transition duration-300 group-hover:scale-125 sm:h-[450px]"
-            />
-
-            <div className="relative pt-3 bg-white">
-              <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                Basic Tee
-              </h3>
-
-              <p className="mt-2">
-                <span className="sr-only"> Regular Price </span>
-
-                <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="#" className="block overflow-hidden group">
-            <img
-              src={men}
-              alt=""
-              className="h-[350px] w-full object-cover transition duration-300 group-hover:scale-125 sm:h-[450px]"
-            />
-
-            <div className="relative pt-3 bg-white">
-              <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                Basic Tee
-              </h3>
-
-              <p className="mt-2">
-                <span className="sr-only"> Regular Price </span>
-
-                <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="#" className="block overflow-hidden group">
-            <img
-              src={men}
-              alt=""
-              className="h-[350px] w-full object-cover transition duration-300 group-hover:scale-125 sm:h-[450px]"
-            />
-
-            <div className="relative pt-3 bg-white">
-              <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                Basic Tee
-              </h3>
-
-              <p className="mt-2">
-                <span className="sr-only"> Regular Price </span>
-
-                <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="#" className="block overflow-hidden group">
-            <img
-              src={men}
-              alt=""
-              className="h-[350px] w-full object-cover transition duration-300 group-hover:scale-125 sm:h-[450px]"
-            />
-
-            <div className="relative pt-3 bg-white">
-              <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                Basic Tee
-              </h3>
-
-              <p className="mt-2">
-                <span className="sr-only"> Regular Price </span>
-
-                <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="#" className="block overflow-hidden group">
-            <img
-              src={men}
-              alt=""
-              className="h-[350px] w-full object-cover transition duration-300 group-hover:scale-125 sm:h-[450px]"
-            />
-
-            <div className="relative pt-3 bg-white">
-              <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                Basic Tee
-              </h3>
-
-              <p className="mt-2">
-                <span className="sr-only"> Regular Price </span>
-
-                <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="#" className="block overflow-hidden group">
-            <img
-              src={men}
-              alt=""
-              className="h-[350px] w-full object-cover transition duration-300 group-hover:scale-125 sm:h-[450px]"
-            />
-
-            <div className="relative pt-3 bg-white">
-              <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                Basic Tee
-              </h3>
-
-              <p className="mt-2">
-                <span className="sr-only"> Regular Price </span>
-
-                <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="#" className="block overflow-hidden group">
-            <img
-              src={men}
-              alt=""
-              className="h-[350px] w-full object-cover transition duration-300 group-hover:scale-125 sm:h-[450px]"
-            />
-
-            <div className="relative pt-3 bg-white">
-              <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                Basic Tee
-              </h3>
-
-              <p className="mt-2">
-                <span className="sr-only"> Regular Price </span>
-
-                <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="#" className="block overflow-hidden group">
-            <img
-              src={men}
-              alt=""
-              className="h-[350px] w-full object-cover transition duration-300 group-hover:scale-125 sm:h-[450px]"
-            />
-
-            <div className="relative pt-3 bg-white">
-              <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                Basic Tee
-              </h3>
-
-              <p className="mt-2">
-                <span className="sr-only"> Regular Price </span>
-
-                <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
-              </p>
-            </div>
-          </a>
-        </div>
+        {products?.map((products) => (
+          <NewDrop products={products} key={products._id}></NewDrop>
+        ))}
       </div>
     </div>
   );
