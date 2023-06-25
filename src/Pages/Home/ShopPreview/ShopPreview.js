acd7f731825eac50a800e9b2bda87cc017dbaefc
FAForhad <faforhad450@gmail.com>
2023-06-25 20:29:29 +0600

    video

--- src/Pages/Home/ShopPreview/ShopPreview.js
+++ src/Pages/Home/ShopPreview/ShopPreview.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./ShopPreview.css";
 import bliss from "../../../assets/Bliss LOGO- Ontor[2].png";
 import Bliss from "../../../assets/Bliss.mov";
+import Bliss20 from "../../../assets/Bliss20.mp4";
 const ShopPreview = () => {
   return (
     <div>
       <section className="relative h-[380px] md:h-[640px] lg:h-[820px] flex flex-col items-center justify-center text-center text-white ">
-        <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+        <div className="hidden md:inline-block  video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
           <video
             src={Bliss}
             className="min-w-full min-h-full absolute object-cover"
@@ -16,7 +17,17 @@ const ShopPreview = () => {
             loop
           ></video>
         </div>
-        <div className="video-content space-y-2 w-24 lg:w-36 z-10">
+        <div className="lg:hidden md:hdden inline-block video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+          <video
+            src={Bliss20}
+            className="min-w-full min-h-full absolute object-cover"
+            type="video/mov"
+            autoPlay
+            muted
+            loop
+          ></video>
+        </div>
+        <div className="hidden md:inline-block video-content space-y-2 w-24 lg:w-36 z-10">
           <img src={bliss} />
         </div>
       </section>
