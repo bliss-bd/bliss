1e4cb943e3e155460863b84f34b0dd137975f114
FAForhad <faforhad450@gmail.com>
2024-01-05 18:49:06 +0600

    updated
    delete some comments

--- src/Pages/Home/Advertiseproduct/AdvertiseProduct.js
+++ src/Pages/Home/Advertiseproduct/AdvertiseProduct.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { TbCurrencyTaka } from "react-icons/tb";
 import { Link } from "react-router-dom";
 const AdvertiseProduct = ({ products }) => {
-  const { name, picture1, price, _id } = products;
+  const { picture1,  _id } = products;
 
 
   return (
@@ -16,16 +15,6 @@ const AdvertiseProduct = ({ products }) => {
         />
 
         <div className="relative pt-3 bg-white">
-          {/* <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-            {name}
-          </h3> */}
-{/* 
-          <p className="mt-2">
-            <span className="sr-only"> Regular Price </span>
-            <span className="tracking-wider text-gray-900 flex items-center">
-              <TbCurrencyTaka></TbCurrencyTaka> {price}
-            </span>
-          </p> */}
         </div>
       </Link>
     </div>
